df0e722065a886bf79490ea02160c87fa4d2f904
BishoySedra <bishosedra0@gmail.com>
2023-08-03 23:14:10 +0300

    V2

--- controllers/task.js
+++ controllers/task.js
@@ -1,3 +1,19 @@
 export function getAllTasks(req, res) {
     res.send('all items');
 }
+
+export function getTask(req, res) {
+    res.send('get single task');
+}
+
+export function editTask(req, res) {
+    res.send('edit task');
+}
+
+export function addTask(req, res) {
+    res.send('add new task');
+}
+
+export function deleteTask(req, res) {
+    res.send('delete task');
+}
--- routes/task.js
+++ routes/task.js
@@ -4,9 +4,9 @@ import * as taskControllers from "../controllers/task.js";
 const router = Router();
 
 router.get(`/`, taskControllers.getAllTasks);
-router.get(`/:id`);
-router.post(`/`);
-router.patch(`/:id`);
-router.delete(`/:id`);
+router.get(`/:id`, taskControllers.getTask);
+router.post(`/`, taskControllers.addTask);
+router.patch(`/:id`, taskControllers.editTask);
+router.delete(`/:id`, taskControllers.deleteTask);
 
 export default router;
